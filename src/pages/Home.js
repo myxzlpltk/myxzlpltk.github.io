8dc12aa1fbb3cd0d8faab5e966bc2fd933637d91
myxzlpltk <59659668+myxzlpltk@users.noreply.github.com>
2021-12-26 21:15:05 +0700

    navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,72 @@
-import React, { Component } from 'react'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Disclosure } from '@headlessui/react';
+import React, { Component, Fragment } from 'react'
+import { Link } from 'react-router-dom';
+import { classNames } from '../utils';
 
-export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                Home
-            </div>
-        )
-    }
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    )
+  }
 }
+
+function Navbar() {
+  const navigation = [
+    { name: 'About Me', href: '#', current: true },
+    { name: 'Portfolio', href: '#', current: false },
+    { name: 'Contacts', href: '#', current: false },
+  ];
+
+  return (
+    <Disclosure as="nav" className="bg-white">
+      {({ open }) => (
+        <Fragment>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-900 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <FontAwesomeIcon icon={faTimes} className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <FontAwesomeIcon icon={faBars} className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  <img src='/logo.svg' alt='Logo' className='h-10' />
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link key={item.name} to={item.href} className={classNames(item.current ? 'bg-gray-900 text-white' : 'text-gray-900 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium')} aria-current={item.current ? 'page' : undefined}>{item.name}</Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Disclosure.Button key={item.name} as="a" href={item.href} className={classNames(item.current ? 'bg-gray-900 text-white' : 'text-gray-900 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium')} aria-current={item.current ? 'page' : undefined}>{item.name}</Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </Fragment>
+      )}
+    </Disclosure>
+  );
+}
+
+export default Home;
