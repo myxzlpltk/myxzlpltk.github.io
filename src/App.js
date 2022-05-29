cbdc32cb09afc0700d010895a04098c5f1b1f146
myxzlpltk <myxzlpltk@gmail.com>
2022-05-29 18:41:19 +0700

    add portal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import Home from "./pages/Home";
+import Portal from "./pages/Portal";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/portal" element={<Portal />} />
         </Routes>
       </div>
     </Router>
