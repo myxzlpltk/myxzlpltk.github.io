fd451c787a4d097b5492f8164cf66bff2b036186
myxzlpltk <myxzlpltk@gmail.com>
2022-05-29 17:21:20 +0700

    update grid formation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -263,7 +263,7 @@ const ArticlesSection = () => {
     <section className="bg-white" id="articles">
       <div className="container py-6 md:py-8 lg:py-10">
         <Text headline4 className="text-left mb-4">Read My Articles</Text>
-        <div className="grid grid-cols-5 gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
           {articles.map((article, i) => (
             <div key={`article-${i}`}>
               <ArticleCard imageUrl={article.imageUrl}
