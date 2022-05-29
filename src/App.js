fbf8f4b574c4de3de35b1a8aa3b6f9fd8d4f79c2
myxzlpltk <myxzlpltk@gmail.com>
2022-05-29 18:55:07 +0700

    convert to hash router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 
 import Home from "./pages/Home";
 import Portal from "./pages/Portal";
 
 export default function App() {
   return (
-    <Router>
+    <HashRouter>
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/portal" element={<Portal />} />
         </Routes>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
