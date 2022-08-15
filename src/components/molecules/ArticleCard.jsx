7ad8fc527d1ceecfca7c26119aec6b699e1fc25f
myxzlpltk <myxzlpltk@gmail.com>
2022-08-15 18:26:31 +0700

    add animation hover

--- src/components/molecules/ArticleCard.jsx
+++ src/components/molecules/ArticleCard.jsx
@@ -13,7 +13,8 @@ ArticleCard.propTypes = {
 function ArticleCard(props) {
   return (
     <a href={props.url} target="_blank" rel="noreferrer">
-      <div className="max-w-sm h-auto mx-auto my-2 rounded overflow-hidden shadow-lg">
+      <div
+        className="max-w-sm h-auto mx-auto my-2 rounded overflow-hidden shadow-lg transition ease-in duration-200 hover:bg-indigo-500 hover:text-white">
         <img className="object-cover aspect-video bg-white" src={props.imageUrl} alt="Volcano" />
         <div className="px-3 py-2 md:px-6 md:py-4">
           <Text body2 className="line-clamp-2 mb-2">{props.title}</Text>
@@ -22,7 +23,7 @@ function ArticleCard(props) {
         <div className="px-3 pb-2 md:px-6 md:pb-2">
           {props.tags.map((tag) => (
             <Text overline
-                  className="inline-block bg-gray-100 hover:bg-gray-200 rounded-md px-3 py-1 text-gray-700 mr-1 mb-1 md:mr-2 md:mb-2">{tag}</Text>
+                  className="inline-block bg-gray-100 hover:bg-gray-200 bg-opacity-50 rounded-md px-3 py-1 text-gray-700 mr-1 mb-1 md:mr-2 md:mb-2">{tag}</Text>
           ))}
         </div>
       </div>
