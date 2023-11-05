b03a87509c6a07ff5f09c5ebc83405c63fa534f0
Saddam Mukti <saddam.coder@gmail.com>
2023-11-05 12:07:00 +0700

    add poetry route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ProjectDetail from "./pages/ProjectDetail";
 import Portal from "./pages/Portal";
 import Swapper from "./pages/Swapper";
 import ModuleDetail from "./pages/ModuleDetail";
+import Poetry from "./pages/Poetry";
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
           <Route path="/modules/:moduleId" element={<ModuleDetail />} />
           <Route path="/portal" element={<Portal />} />
           <Route path="/swapper" element={<Swapper />} />
+          <Route path="/poetry" element={<Poetry />} />
         </Routes>
       </div>
     </HashRouter>
---
+++ src/pages/Poetry.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Poetry = () => {
+  return (
+    <div>Poetry</div>
+  )
+}
+
+export default Poetry
